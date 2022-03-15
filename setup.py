01ad71563e80f212d9273ae962f48b44c11decf9
Silvio Ankermann <silvio@booq.org>
2022-03-15 10:12:47 +0100

    Remove support for Python 3.6 (EOL)

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
